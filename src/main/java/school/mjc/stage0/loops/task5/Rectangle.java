500fb129c57c5655229acf4bf9706447303e6ae3
Sirojiddin Kilichbaev <ssirojiddin2004@gmail.com>
2022-09-17 11:19:58 +0500

    aaa

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
-
+        for (int i = 0; i < height; i++){
+            for (int j = 0; j < length; j++){
+                System.out.print(8);
+            }
+            System.out.println();
+        }
     }
 }
