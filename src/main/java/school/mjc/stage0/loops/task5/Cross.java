0a32ee9586489570727a157958584b5c219e8aa0
Sirojiddin Kilichbaev <ssirojiddin2004@gmail.com>
2022-09-17 11:28:09 +0500

    aaa

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -4,7 +4,7 @@ public class Cross {
     public void printCross(int sideLength) {
         for (int i = 0; i < sideLength; i++){
             for (int j = 0; j < sideLength; j++){
-                if (i == j || i + j == sideLength-1)
+                if (i == sideLength/2 || j == sideLength/2)
                     System.out.print(8);
                 else
                     System.out.print(" ");
